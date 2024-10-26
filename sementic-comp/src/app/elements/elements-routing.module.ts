c732b8d1d655179105a76062ce5731148436a0fe
Phu Vu Phan Quang <phu.vuphanquang@gmail.com>
2024-10-26 16:59:35 +0700

    The Modern Angular Bootcamp: Sementic Comp - Lazy Loading, Widget Module

--- sementic-comp/src/app/elements/elements-routing.module.ts
+++ sementic-comp/src/app/elements/elements-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {path: 'elements', component: ElementsHomeComponent}
+  {path: '', component: ElementsHomeComponent}
 ];
 
 @NgModule({
