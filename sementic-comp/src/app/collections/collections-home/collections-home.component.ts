6cc4da68312ca11786808660818e20910c8c7ee3
Phu Vu Phan Quang <phu.vuphanquang@gmail.com>
2024-10-26 17:01:35 +0700

    The Modern Angular Bootcamp: Sementic Comp - Placeholder, Segment, Tables

--- sementic-comp/src/app/collections/collections-home/collections-home.component.ts
+++ sementic-comp/src/app/collections/collections-home/collections-home.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    {name: 'James', age: 24, job: 'Designer'},
+    {name: 'John', age: 26, job: 'Developer'},
+    {name: 'Amely', age: 24, job: 'Engineer'}
+  ];
+
+  headers = [
+    {key: 'name', label: 'Name'},
+    {key: 'age', label: 'Age'},
+    {key: 'job', label: 'Job'}
+  ]
 
   constructor() { }
 
