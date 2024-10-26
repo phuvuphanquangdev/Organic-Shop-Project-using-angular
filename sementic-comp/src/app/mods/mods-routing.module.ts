5ffa76258516a266cd62a4098222533d657e05c1
Phu Vu Phan Quang <phu.vuphanquang@gmail.com>
2024-10-26 17:06:41 +0700

    The Modern Angular Bootcamp: Sementic Comp - Hooks, Modal

--- sementic-comp/src/app/mods/mods-routing.module.ts
+++ sementic-comp/src/app/mods/mods-routing.module.ts
@@ -1,8 +1,11 @@
+import { ModsHomeComponent } from './mods-home/mods-home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ModsHomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- sementic-comp/src/app/mods/mods.module.ts
+++ sementic-comp/src/app/mods/mods.module.ts
@@ -1,14 +1,18 @@
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ModsRoutingModule } from './mods-routing.module';
+import { ModsHomeComponent } from './mods-home/mods-home.component';
+import { ModalComponent } from './modal/modal.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ModsHomeComponent, ModalComponent],
   imports: [
     CommonModule,
-    ModsRoutingModule
+    ModsRoutingModule,
+    SharedModule
   ]
 })
 export class ModsModule { }
