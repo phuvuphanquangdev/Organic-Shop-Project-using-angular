26c9b5f086a05387577e1fffa1910da9c92f571f
Phu Vu Phan Quang <phu.vuphanquang@gmail.com>
2024-10-26 17:18:36 +0700

    The Modern Angular Bootcamp: Credit Card - Part 1

---
+++ credit-card/src/app/input/input.component.ts
@@ -0,0 +1,17 @@
+import { FormControl } from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+  @Input() control: FormControl;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
