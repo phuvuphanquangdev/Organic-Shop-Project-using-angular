ee859f7d9e24b826a6818b30597305dba10719f7
Phu Vu Phan Quang <phu.vuphanquang@gmail.com>
2024-10-26 19:47:20 +0700

    The Modern Angular Bootcamp: Email Client - Part one

--- email-client/src/app/inbox/inbox-routing.module.ts
+++ email-client/src/app/inbox/inbox-routing.module.ts
@@ -1,10 +1,19 @@
+import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { EmailShowComponent } from './email-show/email-show.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent}
+  { 
+    path: '', 
+    component: HomeComponent,
+    children: [
+      { path: ':id', component: EmailShowComponent},
+      { path: '', component: PlaceholderComponent}
+    ]
+  }
 ];
 
 @NgModule({
